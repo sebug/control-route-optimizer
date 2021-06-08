7285f40bb6400963de60fc52ec6a5e1386f85d4c
sebug <sebastian.gfeller@gmail.com>
2021-06-08 13:47:51 +0200

    Adding Spring starter

---
+++ src/main/java/ch/sebug/controlrouteoptimizer/ControlRouteOptimizerApplication.java
@@ -0,0 +1,13 @@
+package ch.sebug.controlrouteoptimizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlRouteOptimizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlRouteOptimizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/sebug/controlrouteoptimizer/ControlRouteOptimizerApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.sebug.controlrouteoptimizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlRouteOptimizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
